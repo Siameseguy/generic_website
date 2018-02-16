aa0114919a5c84cb573f182ddb6456b3db618f8f
Phanousit Syhakhom <psyhakhom@gmail.com>
2018-02-16 11:34:42 -0500

    added react-router, homepage and login page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import './index.css';
+//import './index.css'
+import 'semantic-ui-css/semtantic.min.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
+)
 registerServiceWorker()
